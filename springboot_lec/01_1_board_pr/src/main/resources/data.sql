1e4ecf0d999831efedad7e34494c769638b852c5
wocks95 <rla1995cks@naver.com>
2025-01-16 17:58:16 +0900

    20250116

---
+++ springboot_lec/01_1_board_pr/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tbl_board VALUES (null, '첫 번째 게시글', '첫 번째 게시글입니다.반갑습니다.', NOW());
+
---
+++ springboot_lec/01_1_board_pr/src/test/java/com/min/app01_1/ApplicationTests.java
@@ -0,0 +1,58 @@
+package com.min.app01_1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.min.app01_1.dto.BoardDto;
+import com.min.app01_1.mapper.IBoardMapper;
+
+@SpringBootTest
+class ApplicationTests {
+  
+  @Autowired
+  private IBoardMapper boardMapper;
+
+	@Test
+	void 매퍼_객체_생성_테스트() {
+	  Assertions.assertNotNull(boardMapper);
+	}
+
+	@Test
+	void now_테스트() {
+	  System.out.println("NOW : " + boardMapper.now());
+	}
+	
+	@Test
+	void board_list_테스트() {
+	  Assertions.assertEquals(1, boardMapper.selectBoardList().size());
+
+	}
+	
+	@Test
+	void board_detail_테스트() {
+	  Assertions.assertEquals("두 번째 게시글", boardMapper.selectBoardById(1).getTitle());
+	}
+	
+	@Test
+	void board_insert_테스트() {
+	  BoardDto boardDto = BoardDto.builder()
+	                           .title("테스트제목")
+	                           .contents("테스트 내용")
+	                           .build();
+	  Assertions.assertEquals(1, boardMapper.insertBoard(boardDto));
+	}
+	
+	@Test
+	void board_update_테스트() {
+	  Assertions.assertEquals(1, boardMapper.updateBoard("수정 블로그", "소주마신건 안비밀",1));
+	}
+	
+	@Test
+	void board_delete_테스트() {
+	  Assertions.assertEquals(1, boardMapper.deleteBoard(1));
+	}
+	
+	
+}
