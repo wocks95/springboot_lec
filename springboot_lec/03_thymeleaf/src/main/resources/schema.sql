2ed0f392f651c68897a5617864310e92132359cd
wocks95 <rla1995cks@naver.com>
2025-01-20 17:47:22 +0900

    20250120

---
+++ springboot_lec/03_thymeleaf/src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS db_boot3;
+CREATE DATABASE IF NOT EXISTS db_boot3;
+USE db_boot3;
+
+DROP TABLE IF EXISTS tbl_board;
+CREATE TABLE IF NOT EXISTS tbl_board
+(
+  board_id INT AUTO_INCREMENT,
+  title    VARCHAR(100) NOT NULL,
+  contents VARCHAR(100),
+  create_dt DATETIME,
+  CONSTRAINT pk_board PRIMARY KEY(board_id)
+) Engine=InnoDB;
+
+
+ ALTER TABLE tbl_board AUTO_INCREMENT = 1001;
